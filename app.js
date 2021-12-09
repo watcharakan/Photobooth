a400e0e8f981848b6f635dd4e67f9937c3d2fd82
pongsakorn <pongsakorn@primo.mobi>
2021-12-09 14:19:58 +0700

    create charge and get qr code

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
-var bat_open = require.resolve('./startDslr.bat');
-var spawn = require('child_process').spawn,
-    ls = spawn('cmd.exe', ['/c', 'my.bat']);
+// var bat_open = require.resolve('./startDslr.bat');
+// var spawn = require('child_process').spawn,
+//     ls = spawn('cmd.exe', ['/c', 'my.bat']);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const omise = require("omise")({
+    publicKey: 'pkey_test_5pvpd2gv9r5m4ak2hq9',
+    secretKey: 'skey_test_5pvpd2gvi814k70o9v3'
+});
+
 const cors = require('cors');
 const corsOptions = {
     origin: 'http://localhost:3000',
@@ -23,16 +32,31 @@ app.get('/', function (req, res) {
 app.get('/open', function (req, res) {
     var today = new Date();
     var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + "." + today.getMilliseconds();
-    console.log('DSLR OPENED AT',time, req.query);
+    console.log('DSLR OPENED AT', time, req.query);
     res.header("Access-Control-Allow-Origin", "*");
-    openDslr()
+    // openDslr()
     res.send('Parameters received: ' + JSON.stringify(req.query));
 });
 
 const openDslr = () => {
-    require('child_process').exec('cmd /c startDslr.bat', function(){
-        // …your callback code may run here…
-     });
+    // require('child_process').exec('cmd /c startDslr.bat', function(){
+    //     // …your callback code may run here…
+    //  });
 }
+
+app.post('/create-payment', async (req, res, next) => {
+    try {
+        const charge = await omise.charges.create({
+            amount: '400000',
+            source: req.body.source,
+            currency: "thb",
+        });
+        res.send({ charge });
+    } catch (err) {
+        console.log(err);
+    }
+    next();
+});
+
 
 app.listen(3000);
--- client/dslr-client/src/components/scanQr/index.js
+++ client/dslr-client/src/components/scanQr/index.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react'
-
+import axios from 'axios';
+import CircularProgress from '@mui/material/CircularProgress';
 export default class ScanQr extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { loading: false, qrImgSrc: '' };
+
+    }
+
+    componentDidMount() {
+        this.createPaymentSource();
+    }
     render() {
         return (
-            <div style={{ display: 'flex', width: '100%', height: '100%', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', color: 'black' }}>
-                <div>QR CODE HERE</div>
+            <div style={{ display: 'flex', width: '100%', height: '100%', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', color: 'black' }}>
+                <div style={{ width: "400px", height: "400px", }}>
+                    {this.state.qrImgSrc === '' && <div style={{ marginTop: '200px' }}><CircularProgress /></div>}
+                    {this.state.qrImgSrc !== '' && <img style={{ marginBottom: '200px' }} src={this.state.qrImgSrc} alt="qrCode" />}
+                </div>
             </div>
         )
     }
+
+    createPaymentSource = () => {
+        const { OmiseCard, Omise } = window
+        Omise.setPublicKey('pkey_test_5pvpd2gv9r5m4ak2hq9');
+        Omise.createSource('promptpay', {
+            "amount": 400000,
+            "currency": "THB"
+        }, (statusCode, response) => {
+            console.log(' response.id', response.id)
+            this.setState({ loading: true })
+            axios.post('http://localhost:3000/create-payment', {
+                source: response.id
+            }).then(res => {
+
+                this.setState({
+                    qrImgSrc: res.data.charge.source.scannable_code.image.download_uri
+                })
+                console.log('qrImgSrc', this.state.qrImgSrc)
+            })
+            console.log(response)
+
+        });
+    }
+
 }
