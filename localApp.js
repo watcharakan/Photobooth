4450a88a843fb3c0236e3bc96ed39a4c59efcb8a
pongsakorn <pongsakorn@primo.mobi>
2022-01-09 00:18:32 +0700

    add cors allow

--- localApp.js
+++ localApp.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200
+}
 
+app.use(cors(corsOptions));
 app.get('/open', function (req, res) {
     var today = new Date();
     var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + "." + today.getMilliseconds();
